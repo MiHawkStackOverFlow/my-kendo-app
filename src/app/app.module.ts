d0691fc686b31d43a16c5a08a16dc2ab8d5a318f
Abhishek Sharma <abhivsharma8@gmail.com>
2020-09-18 21:44:02 +0530

    User list component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { UsersModule } from './app-modules/users/users.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UsersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
